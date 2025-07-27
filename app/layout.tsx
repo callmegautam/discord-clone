5a4d48371a9e73da283debcaf5873ae0d8032979
Gautam Suthar <iamgautamsuthar@outlook.com>
2025-07-27 15:56:41 +0530

    feat: add ThemeProvider component for managing theme context

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const geistSans = Open_Sans({
     variable: "--font-geist-sans",
@@ -25,8 +26,17 @@ export default function RootLayout({
 }>) {
     return (
         <ClerkProvider>
-            <html lang="en">
-                <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+            <html lang="en" suppressHydrationWarning>
+                <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                    <ThemeProvider
+                        attribute={"class"}
+                        defaultTheme="dark"
+                        enableSystem={false}
+                        storageKey="theme"
+                    >
+                        {children}
+                    </ThemeProvider>
+                </body>
             </html>
         </ClerkProvider>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
 import { SignOutButton, UserButton, UserProfile } from "@clerk/nextjs";
 
 export default function Home() {
     return (
-        <div className="bg-black h-screen text-white">
+        <div className=" flex items-center justify-between gap-4 p-4">
             <h1>Home page</h1>
 
-            <UserButton afterSwitchSessionUrl="/" />
+            <div className="flex items-center gap-4">
+                <ModeToggle />
+
+                <UserButton afterSwitchSessionUrl="/" />
+            </div>
         </div>
     );
 }
